20cfd37a631718b4460c9189b8ff6ef743a1ff00
Prashant Singh <prashantsingh@gmail.com>
2026-09-03 11:11:04 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GitPracticeApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
