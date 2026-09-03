f70cf0b5d842678e4a5bf10e16aa977fc5965182
Prashant Singh <prashantsrajput@gmail.com>
2026-09-03 12:36:36 +0530

    merge Prashant with master

---
+++ Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitPracticeApp
+{
+    internal class Class1
+    {
+        public void Fun1()
+        {
+            Console.WriteLine("Hello I am Fun 1 method");
+        }
+    }
+}
