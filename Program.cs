09ec6e1acd6695f235b784470875e06a08a3b429
Prashant Singh <prashantsrajput@gmail.com>
2026-09-03 11:35:29 +0530

    2nd commit

--- Program.cs
+++ Program.cs
@@ -5,7 +5,9 @@
         static void Main(string[] args)
         {
             Console.WriteLine("I am Prashant Singh");
-            Console.WriteLine("I am Anjali SIngh"); ;
+            Console.WriteLine("I am Anjali SIngh");
+            Class1 c = new Class1();
+            c.Fun1();
 
 
         }
